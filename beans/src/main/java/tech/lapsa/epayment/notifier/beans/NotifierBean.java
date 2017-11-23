5d5c46d06313c7df94e88e69768f457290e6233c
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-11-23 15:04:45 +0600

    used

--- beans/src/main/java/tech/lapsa/epayment/notifier/beans/NotifierBean.java
+++ beans/src/main/java/tech/lapsa/epayment/notifier/beans/NotifierBean.java
@@ -8,13 +8,9 @@ import java.util.function.Consumer;
 
 import javax.annotation.Resource;
 import javax.ejb.Stateless;
-import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
+import javax.inject.Inject;
 import javax.jms.Destination;
 import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.MessageProducer;
-import javax.jms.Session;
 
 import tech.lapsa.epayment.domain.Invoice;
 import tech.lapsa.epayment.notifier.NotificationChannel;
@@ -24,12 +20,11 @@ import tech.lapsa.epayment.notifier.Notifier;
 import tech.lapsa.java.commons.function.MyExceptions;
 import tech.lapsa.java.commons.function.MyObjects;
 import tech.lapsa.java.commons.function.MyStrings;
+import tech.lapsa.javax.jms.MyJMSClient;
+import tech.lapsa.javax.jms.MyJMSClient.MyJMSConsumer;
 
 @Stateless
 public class NotifierBean implements Notifier {
-
-    @Resource(name = JNDI_JMS_CONNECTION_FACTORY)
-    private ConnectionFactory connectionFactory;
 
     @Resource(name = JNDI_JMS_DEST_PAYMENTLINK_REQUESTER_EMAIL)
     private Destination paymentLinkUserEmail;
@@ -41,6 +36,9 @@ public class NotifierBean implements Notifier {
     public NotificationBuilder newNotificationBuilder() {
 	return new NotificationBuilderImpl();
     }
+
+    @Inject
+    private MyJMSClient jmsClient;
 
     private final class NotificationBuilderImpl implements NotificationBuilder {
 
@@ -154,25 +152,13 @@ public class NotifierBean implements Notifier {
 	    public void send() {
 		if (sent)
 		    throw new IllegalStateException("Already sent");
-		try (Connection connection = connectionFactory.createConnection()) {
-		    final Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-		    final MessageProducer producer = session.createProducer(destination);
-		    final Message msg = session.createObjectMessage(invoice);
-		    properties.entrySet() //
-			    .stream() //
-			    .forEach(x -> {
-				try {
-				    msg.setStringProperty(x.getKey(), x.getValue());
-				} catch (final JMSException e) {
-				    throw new RuntimeException("Failed to assign a property", e);
-				}
-			    });
-
-		    producer.send(msg);
+		try {
+		    MyJMSConsumer<Invoice> consumer = jmsClient.createConsumer(destination);
+		    consumer.accept(invoice);
 		    sent = true;
 		    if (MyObjects.nonNull(onSuccess))
 			onSuccess.accept(this);
-		} catch (final JMSException e) {
+		} catch (final JMSException | RuntimeException e) {
 		    throw new RuntimeException("Failed to assign a notification task", e);
 		}
 	    }
