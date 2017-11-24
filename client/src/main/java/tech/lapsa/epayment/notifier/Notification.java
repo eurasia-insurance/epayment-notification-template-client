886f36f2c65456384d8ef7fe34ed9cf175a7046b
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-11-24 18:55:13 +0600

    changed

---
+++ client/src/main/java/tech/lapsa/epayment/notifier/Notification.java
@@ -0,0 +1,79 @@
+package tech.lapsa.epayment.notifier;
+
+import java.io.Serializable;
+
+import tech.lapsa.epayment.domain.Invoice;
+import tech.lapsa.java.commons.function.MyObjects;
+
+public final class Notification implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    public static NotificationBuilder builder() {
+	return new NotificationBuilder();
+    }
+
+    public static final class NotificationBuilder {
+
+	private NotificationChannel channel;
+	private NotificationRecipientType recipientType;
+	private NotificationRequestStage event;
+	private Invoice entity;
+
+	private NotificationBuilder() {
+	}
+
+	public NotificationBuilder withChannel(final NotificationChannel channel) {
+	    this.channel = MyObjects.requireNonNull(channel, "channel");
+	    return this;
+	}
+
+	public NotificationBuilder withRecipient(final NotificationRecipientType recipientType) {
+	    this.recipientType = MyObjects.requireNonNull(recipientType, "recipientType");
+	    return this;
+	}
+
+	public NotificationBuilder withEvent(final NotificationRequestStage event) {
+	    this.event = MyObjects.requireNonNull(event, "event");
+	    return this;
+	}
+
+	public NotificationBuilder forEntity(final Invoice entity) {
+	    this.entity = MyObjects.requireNonNull(entity, "entity");
+	    return this;
+	}
+
+	public Notification build() {
+	    return new Notification(channel, recipientType, event, entity);
+	}
+    }
+
+    private final NotificationChannel channel;
+    private final NotificationRecipientType recipientType;
+    private final NotificationRequestStage event;
+    private final Invoice entity;
+
+    private Notification(NotificationChannel channel, NotificationRecipientType recipientType,
+	    NotificationRequestStage event, Invoice entity) {
+	this.channel = MyObjects.requireNonNull(channel, "channel");
+	this.recipientType = MyObjects.requireNonNull(recipientType, "recipientType");
+	this.event = MyObjects.requireNonNull(event, "event");
+	this.entity = MyObjects.requireNonNull(entity, "entity");
+    }
+
+    public NotificationChannel getChannel() {
+	return channel;
+    }
+
+    public NotificationRecipientType getRecipientType() {
+	return recipientType;
+    }
+
+    public NotificationRequestStage getEvent() {
+	return event;
+    }
+
+    public Invoice getEntity() {
+	return entity;
+    }
+}
--- client/src/main/java/tech/lapsa/epayment/notifier/Notifier.java
+++ client/src/main/java/tech/lapsa/epayment/notifier/Notifier.java
@@ -1,45 +1,9 @@
 package tech.lapsa.epayment.notifier;
 
-import java.util.function.Consumer;
+import javax.ejb.Local;
 
-import tech.lapsa.epayment.domain.Invoice;
-
+@Local
 public interface Notifier {
 
-    @Deprecated
-    default void assignOrderNotification(NotificationChannel channel, NotificationRecipientType recipientType,
-	    NotificationRequestStage stage,
-	    Invoice invoice) {
-	newNotificationBuilder() //
-		.withChannel(channel) //
-		.withEvent(stage) //
-		.withRecipient(recipientType) //
-		.forEntity(invoice) //
-		.build() //
-		.send();
-    }
-
-    NotificationBuilder newNotificationBuilder();
-
-    interface NotificationBuilder {
-
-	NotificationBuilder withChannel(NotificationChannel channel);
-
-	NotificationBuilder withRecipient(NotificationRecipientType recipientType);
-
-	NotificationBuilder withEvent(NotificationRequestStage stage);
-
-	NotificationBuilder forEntity(Invoice invoice);
-
-	NotificationBuilder withProperty(String name, String value);
-
-	Notification build();
-
-	interface Notification {
-
-	    Notification onSuccess(Consumer<Notification> cons);
-
-	    void send();
-	}
-    }
+    void send(Notification notification);
 }
